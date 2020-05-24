2d16917d77d95db861d568cf1a90187831378053
bannzai <kingkong999yhirose@gmail.com>
2020-05-24 20:48:21 +0900

    :recycle: setting root to pages#home

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-  def hello
-    render html: 'Hello, world'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   get 'pages/about'
-  root 'application#hello'
+  root 'pages#home'
 end
