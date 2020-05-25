d63fe1bbf34eee16cb00f8bd0f20cc90afea5402
bannzai <kingkong999yhirose@gmail.com>
2020-05-25 09:19:11 +0900

    :recycle: render home with css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,10 @@
 
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
+require("jquery");
+require("bootstrap");
 
-import 'bootstrap';
-import '../stylesheets/application';
+import '../stylesheets/application.scss';
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'pages/about'
+  get 'pages/home'
   root 'pages#home'
 end
