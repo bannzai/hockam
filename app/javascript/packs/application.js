15ab8bc45253f9945721c5940caad6f22ac7c394
bannzai <kingkong999yhirose@gmail.com>
2020-06-02 22:23:51 +0900

    :memo: define webpacker images

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,8 @@ require("jquery");
 require("bootstrap");
 
 import '../stylesheets/application.scss';
+const images = require.context('../images/', true)
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
