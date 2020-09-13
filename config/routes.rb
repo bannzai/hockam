bb9102e6b90c550e635726a65189ca9cef5aaacb
bannzai <kingkonog999yhirose@gmail.com>
2020-09-13 21:16:05 +0900

    :shell: be rails generate administrate:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :suzuri_goods
+      resources :minne_goods
+
+      root to: "suzuri_goods#index"
+    end
   get 'goods/minne'
   get 'goods/suzuri'
   get 'goods/stores'
