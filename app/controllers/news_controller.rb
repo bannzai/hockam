ca6cdb9a9326552690e1cc5ac89273f27241aa93
bannzai <kingkonog999yhirose@gmail.com>
2020-10-04 17:22:28 +0900

    :sparkles: add pagination

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,6 @@
 class NewsController < ApplicationController
   def index
-    @news_list = News.all
+    @news_list = News.where(:is_hidden => false).order('priority DESC').page(params[:page]).per(15)
   end
 
   def show
