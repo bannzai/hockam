e76a5684d38c7fc83e8ab1994f8d18b841ed60f9
bannzai <kingkong999yhirose@gmail.com>
2020-10-10 08:53:51 +0900

    :sparkels: fetch data from AR

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
   def home
+    @minne_goods = MinneGood.where(:is_hidden => false).order('item_id DESC').take(20)
+    @suzuri_goods = SuzuriGood.where(:is_hidden => false).order('item_id DESC').take(20)
+    @news_list = News.where(:is_hidden => false).order('priority DESC').take(20)
     render :home
   end
 end
