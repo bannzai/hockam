ca14dc6c877c7413ba133b2c88f2bb7a6782be81
bannzai <kingkong999yhirose@gmail.com>
2020-10-10 12:45:36 +0900

    :memo: missing specifier jquery environment

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,8 @@ const webpack = require('webpack')
 environment.plugins.prepend(
    'Provide',
    new webpack.ProvidePlugin({
-       $: 'jquery/src/jquery',
-       jQuery: 'jquery/src/jquery',
+       $: 'jquery',
+       jQuery: 'jquery',
        Popper: 'popper.js'
    })
 )
