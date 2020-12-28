80348d49cf53f4951cbcf64697471230d622b0ca
bannzai <kingkong999yhirose@gmail.com>
2020-12-28 14:24:05 +0900

    :recycle: fix maximum count of news

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     minne_goods = MinneGood.where(:is_hidden => false).order('item_id DESC').take(20)
     suzuri_goods = SuzuriGood.where(:is_hidden => false).order('item_id DESC').take(20)
     @goods = combine(minne_goods, suzuri_goods)
-    @news_list = News.where(:is_hidden => false).order('priority DESC').take(20)
+    @news_list = News.where(:is_hidden => false).order('priority DESC').take(8)
     render :home
   end
 
