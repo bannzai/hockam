7b5e183fcec5eda37df4c638dfd4ceffa773d2f9
bannzai <kingkong999yhirose@gmail.com>
2020-12-29 17:11:56 +0900

    :memo: add smtp settings for development environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,4 +52,16 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  587,
+    domain:               'gmail.com',
+    user_name:             ENV['INQUIRY_MAIL_ADDRESS'],
+    password:              ENV['INQUIRY_MAIL_PASSWJORD'],
+    authentication:       'plain',
+    enable_starttls_auto:  true
+  }
+
 end
