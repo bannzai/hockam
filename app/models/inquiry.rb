3d22b9eaa5d87e9780d4bc73f719c28239cdd053
bannzai <kingkong999yhirose@gmail.com>
2020-12-30 12:13:08 +0900

    :spakrles: define model validator
    #
    # ==== Emojis ====
    # 🐛  :bug: バグ修正
    # 🤘  :metal: 機能改善
    # 🐚  :shell: コマンドを実行した結果とか
    # ✨  :sparkles: 部分的な機能追加
    # 🎉  :tada: 盛大に祝うべき大きな機能追加
    # 💉  :syringe: libraryの追加
    # 📝  :memo: ドキュメント・READMEの整備
    # ♻️   :recycle: リファクタリング
    # 🚿  :shower: 不要な機能・使われなくなった機能の削除
    # 💚  :green_heart: テストやCIの修正・改善
    # 👕  :shirt: Lintエラーの修正やコードスタイルの修正
    # 🚀  :rocket: パフォーマンス改善
    # 🆙  :up: 依存パッケージなどのアップデート
    # 🔒  :lock: 新機能の公開範囲の制限
    # 👮  :cop: セキュリティ関連の改善
    # 🎨  :art: デザイン修正
    # 🚧  :construction: WIP 一時的なコミット
    
    # ==== Format ====
    # :emoji: Subject
    #
    # Commit body...
    
    # ==== The Seven Rules ====
    # 1. Separate subject from body with a blank line
    # 2. Limit the subject line to 50 characters
    # 3. Capitalize the subject line
    # 4. Do not end the subject line with a period
    # 5. Use the imperative mood in the subject line
    # 6. Wrap the body at 72 characters
    # 7. Use the body to explain what and why vs. how
    #
    # How to Write a Git Commit Message http://chris.beams.io/posts/git-commit/
    
    # reference from http://memo.goodpatch.co/2016/07/beautiful-commits-with-emojis/
    # for https://github.com/bannzai/commit_template

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,2 +1,8 @@
 class Inquiry < ApplicationRecord
+	attr_accessor :sender_name, :sender_name_reading, :sender_mail_address, :message
+
+	validates :sender_name, :presence => {:message => 'お名前を入力してください' }
+	validates :sender_name_reading, :presence => {:message => 'お名前を入力してください' }
+	validates :sender_mail_address, :presence => {:message => 'メールアドレスを入力してください' }
+	validates :message, :presence => {:message => '内容を入力してください' }
 end
