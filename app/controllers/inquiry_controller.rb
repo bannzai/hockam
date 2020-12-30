a87ab86697909cb0dd66034bd7ec657e2081cbf5
bannzai <kingkong999yhirose@gmail.com>
2020-12-30 12:29:30 +0900

    :sparkles: fix controller actions

--- app/controllers/inquiry_controller.rb
+++ app/controllers/inquiry_controller.rb
@@ -3,7 +3,18 @@ class InquiryController < ApplicationController
     @inquiry = Inquiry.new
     render template: 'inquiry/form'
   end
+
   def confirm
-    render template: 'inquiry/confim'
+    @inquiry = Inquiry.new(inquiry_params)
+    if @inquiry.valid?
+      render action: 'confirm'
+    else
+      render action: 'form'
+    end
+  end
+
+  private
+  def inquiry_params
+    params.require(:inquiry).permit(:sender_name, :sender_name_reading ,:sender_mail_address, :message)
   end
 end
